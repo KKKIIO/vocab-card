ef3467dda8065925408af3ebf653c838811f049b
kkkiio <xc130d210@outlook.com>
2023-10-23 16:00:56 +0000

    fix(auth): use token.sub instead of token.id

--- lib/auth.ts
+++ lib/auth.ts
@@ -6,24 +6,37 @@ import type {
   NextApiResponse,
 } from "next";
 import { NextAuthOptions, getServerSession } from "next-auth";
+import { Provider } from "next-auth/providers";
+import EmailProvider from "next-auth/providers/email";
 import GoogleProvider from "next-auth/providers/google";
+
+const providers: Provider[] = [
+  GoogleProvider({
+    clientId: process.env.GOOGLE_CLIENT_ID!,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+  }),
+];
+
+if (process.env.NODE_ENV !== "production") {
+  providers.push(
+    EmailProvider({
+      server: process.env.EMAIL_SERVER,
+      from: process.env.EMAIL_FROM,
+    })
+  );
+}
 
 export const authOptions = {
   adapter: PrismaAdapter(prisma),
   session: {
     strategy: "jwt", // nextauth middleware only support jwt: https://next-auth.js.org/configuration/nextjs#caveats
   },
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID!,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-    }),
-  ],
+  providers,
   callbacks: {
-    async session({ session, token, user }) {
+    async session({ session, token }) {
       // Send properties to the client, like an access_token and user id from a provider.
       // session.accessToken = token.accessToken;
-      session.user!.id = token.id as string;
+      session.user!.id = token.sub!; // WARN: next-auth docs use token.id, but it's not available
 
       return session;
     },
@@ -62,6 +75,9 @@ export async function authenticatedUser(
   const session = await auth(...args);
   if (!session.user) {
     throw new Error("No user found");
+  }
+  if (!session.user.id) {
+    throw new Error("Invalid user id");
   }
   return session.user;
 }
