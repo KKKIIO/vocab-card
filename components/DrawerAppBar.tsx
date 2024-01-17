d17b0ab7415d94dbd6c22b32d28f5f5e8b962efe
kkkiio <xc130d210@outlook.com>
2024-01-17 16:47:09 +0000

    fix(app): make appbar responsive

---
+++ components/DrawerAppBar.tsx
@@ -0,0 +1,97 @@
+"use client";
+import { Home, Lightbulb } from "@mui/icons-material";
+import MenuIcon from "@mui/icons-material/Menu";
+import { ListItemIcon } from "@mui/material";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Divider from "@mui/material/Divider";
+import Drawer from "@mui/material/Drawer";
+import IconButton from "@mui/material/IconButton";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemText from "@mui/material/ListItemText";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Link from "next/link";
+import { useState } from "react";
+import { UserAvatar } from "./UserAvatar";
+
+const drawerWidth = 240;
+const LINKS = [
+  { text: "Home", href: "/", icon: Home },
+  { text: "Cards", href: "/cards", icon: Lightbulb },
+];
+const title = "Vocab Card";
+
+export function DrawerAppBar({
+  user,
+}: {
+  user: { name?: string | null | undefined; email?: string | null | undefined };
+}) {
+  const [open, setOpen] = useState(false);
+
+  const handleDrawerToggle = () => {
+    setOpen((prevState) => !prevState);
+  };
+
+  return (
+    <>
+      <AppBar component="nav">
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            onClick={handleDrawerToggle}
+            // sx={{ mr: 2, display: { sm: "none" } }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" component="div">
+            {title}
+          </Typography>
+
+          <Box sx={{ marginLeft: "auto" }}>
+            <UserAvatar user={user} />
+          </Box>
+        </Toolbar>
+      </AppBar>
+      <nav>
+        <Drawer
+          variant="temporary"
+          open={open}
+          onClose={handleDrawerToggle}
+          ModalProps={{
+            keepMounted: true, // Better open performance on mobile.
+          }}
+          sx={{
+            "& .MuiDrawer-paper": {
+              boxSizing: "border-box",
+              width: drawerWidth,
+            },
+          }}
+        >
+          <Box onClick={handleDrawerToggle} sx={{ textAlign: "center" }}>
+            <Typography variant="h6" sx={{ my: 2 }}>
+              {title}
+            </Typography>
+            <Divider />
+            <List>
+              {LINKS.map(({ text, href, icon: Icon }) => (
+                <ListItem key={href} disablePadding>
+                  <ListItemButton component={Link} href={href}>
+                    <ListItemIcon>
+                      <Icon />
+                    </ListItemIcon>
+                    <ListItemText primary={text} />
+                  </ListItemButton>
+                </ListItem>
+              ))}
+            </List>
+          </Box>
+        </Drawer>
+      </nav>
+    </>
+  );
+}
