b58dcf33a77783f4f6080b714ceca2405030a68a
kkkiio <xc130d210@outlook.com>
2024-03-16 02:47:29 +0000

    feat(source): support add source

--- app/api/sources/core.ts
+++ app/api/sources/core.ts
@@ -3,22 +3,32 @@ import prisma from "lib/prisma";
 
 export type CreateSourceArgs = {
   deskId: number;
-  name: string; url: string;
+  name: string;
+  url: string;
 }
 
 const nonLocationParams = ["utm_source", "utm_medium", "utm_term"];
+
+export async function createSource({
+  deskId,
+  name, url,
+}: CreateSourceArgs) {
+  url = cleanUpUrl(url);
+
+  return await prisma.source.create({
+    data: {
+      deskId,
+      name,
+      url,
+    },
+  })
+}
 
 export async function upsertSource({
   deskId,
   name, url,
 }: CreateSourceArgs) {
-  const urlObject = new URL(url);
-  if (nonLocationParams.some((param) => urlObject.searchParams.has(param))) {
-    for (const param of nonLocationParams) {
-      urlObject.searchParams.delete(param);
-    }
-    url = urlObject.toString();
-  }
+  url = cleanUpUrl(url);
 
   return await prisma.source.upsert({
     where: { deskId_url: { deskId, url: url } },
@@ -29,4 +39,15 @@ export async function upsertSource({
       url,
     },
   })
+}
+
+function cleanUpUrl(url: string) {
+  const urlObject = new URL(url);
+  if (nonLocationParams.some((param) => urlObject.searchParams.has(param))) {
+    for (const param of nonLocationParams) {
+      urlObject.searchParams.delete(param);
+    }
+    url = urlObject.toString();
+  }
+  return url;
 }
